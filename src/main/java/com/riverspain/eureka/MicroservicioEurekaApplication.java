03d026056b6442dcb3a2bfb6e25cc1d391034f2c
JoseR <josequispeogosi12927@gmail.com>
2025-05-15 21:20:49 +0200

    deploy 11

--- src/main/java/com/riverspain/eureka/MicroservicioEurekaApplication.java
+++ src/main/java/com/riverspain/eureka/MicroservicioEurekaApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    SystemMetricsAutoConfiguration.class,
+    MetricsAutoConfiguration.class
+})
 public class MicroservicioEurekaApplication {
 
 	public static void main(String[] args) {
